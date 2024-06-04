d8d07b2d06798e73b5e59acc1282cd2c1e235200
Surabhi NH <aromanewville@gmail.com>
2024-06-05 02:40:42 +0530

    changes to rabs, routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { TodosComponent } from './todos/todos.component';
+import { NotesComponent } from './notes/notes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: 'bookmarks', component:BookmarksComponent },
+   { path: 'todos', component:TodosComponent },
+   { path: 'notes', component:NotesComponent }
+];

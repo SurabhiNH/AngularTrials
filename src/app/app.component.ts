33ac79b7d64def463605f275283311ee03e5fe12
Surabhi NH <aromanewville@gmail.com>
2024-06-05 23:37:58 +0530

    Time and date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import { TabsComponent } from "./tabs/tabs.component";
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, TabsComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, TabsComponent]
 })
 export class AppComponent {
   title = 'MyAngularHub';
+  timeHoursNow: any = new Date().getHours();
+  timeMinutesNow: any = new Date().getMinutes();
+  currentDate: any = new Date().toString();
+  arrays = [];
+
+  ngOnInit() {
+    //Wed,Jun,05,2024,20:32:00,GMT+0530,(India,Standard,Time)
+    this.arrays = this.currentDate.split(' ');
+    this.currentDate = this.arrays[2] + " " + this.arrays[1]+ " " + this.arrays[3];
+  }
 }
